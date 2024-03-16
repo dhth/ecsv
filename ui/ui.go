0f601e5a3521be4d44799d803c7b2640181bb817
Dhruv Thakur <dhruvt93@gmail.com>
2024-03-16 19:14:53 +0100

    fix: don't use input if -format provided

--- ui/ui.go
+++ ui/ui.go
@@ -19,7 +19,10 @@ func RenderUI(envSequence []string, systems []System, outFormat OutFormat) {
 
 	var opts []tea.ProgramOption
 	if outFormat != UnspecifiedFmt {
-		opts = []tea.ProgramOption{tea.WithoutRenderer()}
+		opts = append(opts, tea.WithoutRenderer())
+        // TODO: this may be a hack, and will prevent using STDIN for
+        // CLI mode, find a better way
+		opts = append(opts, tea.WithInput(nil))
 	}
 	p := tea.NewProgram(newModel(envSequence, systems, outFormat), opts...)
 	if _, err := p.Run(); err != nil {
